67c113f06510df6295fd44130e425e3f3ea6831d
jeka2020a <evgenzekul@gmail.com>
2024-06-21 11:11:01 +0300

    second comm

--- assets/script/add-item.js
+++ assets/script/add-item.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const add__item = document.querySelector('.category__pulse-1');
     const item = document.querySelector('.category__item');
+    const body = document.querySelector('.body');
   
     add__item.addEventListener('click' , () => {
         item.style = "opacity: 1";
+    });
 
-    })
+
 
     
 })
