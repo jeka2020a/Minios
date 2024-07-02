064acdcdaf3a4aaffc3281c3cb88085cba6fb5da
jeka2020a <evgenzekul@gmail.com>
2024-07-02 16:13:07 +0300

    update sidebar

--- assets/script/sidebar.js
+++ assets/script/sidebar.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const close = document.querySelector('.header__sidebar-close');
     const sidebar = document.querySelector('.header__sidebar');
     const login = document.querySelector('.header__sidebar-login');
+    const main__tab = document.querySelector('.main__li');
+    const main__list = document.querySelector('.main__list');
+    const main__title = document.querySelector('.main__li-a');
 
   
     open__sidebar.addEventListener('click' , () => {
@@ -19,4 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
         close.style = "display: none";
         login.style = "display: none";
     })
+/*
+    main__tab.addEventListener('click' , () => {
+        main__title.style = "display: none;";
+        document.querySelector(".main__li").classList.remove(".main__li::after");
+        //main__tab.classList.remove(".main__li::after");
+    })
+*/
+
 })
