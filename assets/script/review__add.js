e6e459574bea221eaedf3f9b0094b659a374ee65
jeka2020a <evgenzekul@gmail.com>
2024-07-04 21:18:31 +0300

    update

--- assets/script/review__add.js
+++ assets/script/review__add.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const close__sidebar__review = document.querySelector('.description__button-second');
     const review__shadow = document.querySelector('.review__shadow');
     const review__close = document.querySelector('.review__close');
+    const review__close1 = document.querySelector('.review__close1');
+    const review__close2 = document.querySelector('.review__close2');
     const review = document.querySelector('.review');
 
     add__sidebar__review.addEventListener('click' , () => {
@@ -15,4 +17,15 @@ document.addEventListener('DOMContentLoaded', () => {
         review__shadow.style = "width: 0%";
         review.style = "right: -50%";
     });
+
+
+    review__close1.addEventListener('click' , () => {
+        review__shadow.style = "width: 0%";
+        review.style = "right: -70%";
+    });
+    review__close2.addEventListener('click' , () => {
+        review__shadow.style = "width: 0%";
+        review.style = "right: -100%";
+    });
+
 })
